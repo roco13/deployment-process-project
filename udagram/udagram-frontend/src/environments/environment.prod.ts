db3d735af881ac08f2d70131c164a921d049b0d9
rgiro <rfernandezgiro@pch.com>
2024-05-08 16:57:12 -0400

    test lint

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -8,7 +8,7 @@ export const environment = {
   apiHost: 'https://Udagram-api-3-env.eba-aaqcp3ai.us-east-1.elasticbeanstalk.com/api/v0',
 };
 
-/* 
+/*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
  *
